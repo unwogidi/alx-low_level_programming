a7265bd1aabca0a3d55483ddd5cace2e509354cc
Stanley Nwogidi <unwogidi@gmail.com>
2024-06-22 05:00:03 -0700

    C program that use write() to print a strings of char

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include <unistd.h>
 
 /**
- *main - Entry point
+ * main - Entry point
  *
- *Return: Always 1 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-const char msg;
-*msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, msg, 59);
+char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, msg, sizeof(msg) - 1);
 return (1);
 }
