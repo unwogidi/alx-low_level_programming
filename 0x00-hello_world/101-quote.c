f0a034c92e3372679f25d1aa526ac53ace3c1b8f
Stanley Nwogidi <unwogidi@gmail.com>
2024-06-22 04:14:51 -0700

    C program that use write() to print a strings of char

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-  const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  write(2, msg, 59);
-  return (1);
+const char msg;
+*msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, msg, 59);
+return (1);
 }
