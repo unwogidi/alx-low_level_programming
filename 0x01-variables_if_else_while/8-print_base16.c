43c89906801af30b2d7c4683035015f505c320ff
Stanley Nwogidi <unwogidi@gmail.com>
2024-06-23 15:36:26 -0700

    prints numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print all the numbers of base 16 in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch;
+for (ch = '0'; ch <= '9'; ch++)
+{
+putchar(ch);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
