8303f3e713e2bb8ab22146190d8933bae2e744d6
Stanley Nwogidi <unwogidi@gmail.com>
2024-06-23 04:00:29 -0700

    C code to print the last digit of a number and consider some functions

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 }
 else
 {
-printf("last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 }
 return (0);
 }
