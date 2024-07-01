a7680fbbcdfa82b39b7722b8d56bb770892d2d0b
Stanley Nwogidi <unwogidi@gmail.com>
2024-07-01 15:00:09 -0700

    function - prints a string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,6 +2,24 @@
 #include <unistd.h>
 
 /**
+ * _strlen - returns the length of a string
+ * @s: pointer to the string
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+
+
+/**
  * print_rev - prints a string in reverse with a new line
  * @s: string to be printed in reverse
  */
