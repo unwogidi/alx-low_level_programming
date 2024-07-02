c842b109bee0497f6c5d2989044f467ff37c5893
Stanley Nwogidi <unwogidi@gmail.com>
2024-07-02 06:27:53 -0700

    function - convert a string to an integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: the string to convert
+ * Return: the converted integer
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int started = 0;
+
+	while (*s)
+	{
+		if (*s == '-' && !started)
+		{
+			sign *= -1;
+		}
+		else if (*s == '+' && !started)
+		{
+			/* ignore the plus sign */
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			started = 1;
+			result = result * 10 + (*s - '0');
+		}
+		else if (started)
+		{
+			break;
+		}
+		s++;
+	}
+
+	return (sign * result);
+}
