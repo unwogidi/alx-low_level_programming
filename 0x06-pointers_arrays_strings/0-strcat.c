6abbe697b3323b57cbbeb7cc5fe1ccec91a7e1ce
Stanley Nwogidi <unwogidi@gmail.com>
2024-07-04 11:15:33 -0700

    function - concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,6 +5,7 @@
  * _strcat - concatenates two strings
  * @dest: destination string
  * @src: source string
+ * Return: a pointer to the resulting string dest
  */
 char *_strcat(char *dest, char *src)
 {
