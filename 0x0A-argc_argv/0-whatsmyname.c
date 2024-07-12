502dc530f4fe87036874163156a839861203b2c9
Stanley Nwogidi <unwogidi@gmail.com>
2024-07-12 15:05:19 -0700

    program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,8 +2,8 @@
 
 /**
  * main - a program that prints its name
- * @argc - size of array
- * @argv - array
+ * @argc: size of array
+ * @argv: array
  * Return: 0 Success
  */
 int main(int argc, char *argv[])
