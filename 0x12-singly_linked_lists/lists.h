c8c8bd7623ec4b1f3b3188e784e8cd4902bdbfcd
Stanley Nwogidi <unwogidi@gmail.com>
2024-08-02 10:34:07 -0700

    function that frees a list_t list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -24,5 +24,6 @@ list_t *add_node(list_t **head, const char *str);
 char *_strdup(const char *s);
 unsigned int _strlen(const char *s);
 list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif /* LISTS_H */
